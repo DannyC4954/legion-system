c139ae61bad0e0c456ae2ebdbbf99684e072eb0d
Danny Clayton <49194062+DannyC4954@users.noreply.github.com>
2021-08-06 11:36:24 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,19 +3,17 @@ $(document).ready(function(){
     // Drop down search
     $('#show-search-bar').on('click', function(){
         $('.search-container').toggle();
-        // Change icon depending on toggle 
-    if($('.search-container').css('display') == 'block') {
-        console.log('success');
-        $('#show-search-bar').removeClass('fas');
-        $('#show-search-bar').removeClass('fa-search');
-        $('#show-search-bar').addClass('far');
-        $('#show-search-bar').addClass('fa-times-circle');
-    } else {
-        console.log('fail');
-        $('#show-search-bar').addClass('fas');
-        $('#show-search-bar').addClass('fa-search');
-        $('#show-search-bar').removeClass('far');
-        $('#show-search-bar').removeClass('fa-times-circle');
-    }
+        // Change icon depending on toggle
+      if($('.search-container').css('display') == 'block') {
+          $('#show-search-bar').removeClass('fas');
+          $('#show-search-bar').removeClass('fa-search');
+          $('#show-search-bar').addClass('far');
+          $('#show-search-bar').addClass('fa-times-circle');
+      } else {
+          $('#show-search-bar').addClass('fas');
+          $('#show-search-bar').addClass('fa-search');
+          $('#show-search-bar').removeClass('far');
+          $('#show-search-bar').removeClass('fa-times-circle');
+      }
     });
 });
